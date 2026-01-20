363084cf4b33d27f59e17830c15d522c40da6f43
sariiev <s30244@pjwstk.edu.pl>
2026-01-20 22:01:29 +0100

    add installation of Java and Spark

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,27 @@
 FROM apache/airflow:3.1.6
 
+USER root
+
+RUN apt-get update && apt-get install -y \
+    openjdk-17-jdk \
+    wget \
+    procps \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+ENV SPARK_VERSION=3.5.0
+ENV HADOOP_VERSION=3
+RUN wget -q https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz \
+    && tar -xzf spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz -C /opt \
+    && rm spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz \
+    && ln -s /opt/spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION} /opt/spark
+
+ENV JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64
+ENV SPARK_HOME=/opt/spark
+ENV PATH=$PATH:$JAVA_HOME/bin:$SPARK_HOME/bin:$SPARK_HOME/sbin
+ENV PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9.7-src.zip:$PYTHONPATH
+
+USER airflow
+
 COPY airflow/requirements.txt /requirements.txt
 RUN pip install --no-cache-dir -r /requirements.txt
---
+++ airflow/spark_jobs/transformation.py
@@ -0,0 +1,36 @@
+from pyspark.sql import SparkSession
+
+import sys
+
+from pyspark.sql.functions import to_date, col
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+spark = SparkSession.builder.appName("Transformation").getOrCreate()
+
+df = spark.read.csv(input_path, header=True, inferSchema=True)
+
+rows = df.count()
+
+df_clean = df.filter(
+    (df.monthly_spend >= 0) &
+    (df.weekly_purchases >= 0) &
+    (df.account_age_months >= 0)
+)
+
+new_rows = df_clean.count()
+
+print(f"Removed {rows-new_rows} invalid rows")
+
+df_clean = df_clean.withColumn("last_purchase_date", to_date("last_purchase_date", "yyyy-MM-dd"))
+print("Converted last_purchase_date to datetime")
+
+bool_columns = ["has_children", "loyalty_program_member", "weekend_shopper", "premium_subscription"]
+for column in bool_columns:
+    df_clean = df_clean.withColumn(column, col(column).cast("boolean"))
+    print(f"Converted {column} to bool")
+
+df_clean.write.mode("overwrite").csv(output_path)
+
+spark.stop()
