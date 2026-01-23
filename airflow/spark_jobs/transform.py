984c93e7a06faae64bfb95bb0a11d6c2027ed8a0
sariiev <s30244@pjwstk.edu.pl>
2026-01-23 11:17:12 +0100

    replace writing to parquet with writing to postgres

--- airflow/spark_jobs/transform.py
+++ airflow/spark_jobs/transform.py
@@ -1,8 +1,22 @@
+import os
+
 from pyspark.sql import SparkSession
 
 import sys
 
 from pyspark.sql.functions import to_date, col
+
+POSTGRES_HOST = os.environ["POSTGRES_HOST"]
+POSTGRES_PORT = os.environ["POSTGRES_PORT"]
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+
+WAREHOUSE_DB = os.environ["WAREHOUSE_DB"]
+WAREHOUSE_SCHEMA_RAW = os.environ["WAREHOUSE_SCHEMA_RAW"]
+DATASET_NAME = os.environ["DATASET_NAME"]
+
+jdbc_url = f"jdbc:postgresql://{POSTGRES_HOST}:{POSTGRES_PORT}/{WAREHOUSE_DB}"
+table_name = f"{WAREHOUSE_SCHEMA_RAW}.{DATASET_NAME}_clean"
 
 input_path = sys.argv[1]
 output_path = sys.argv[2]
@@ -31,6 +45,13 @@ for column in bool_columns:
     df_clean = df_clean.withColumn(column, col(column).cast("boolean"))
     print(f"Converted {column} to bool")
 
-df_clean.write.mode("overwrite").parquet(output_path)
+df_clean.write.format("jdbc")\
+    .option("url", jdbc_url)\
+    .option("dbtable", table_name)\
+    .option("user", POSTGRES_USER)\
+    .option("password", POSTGRES_PASSWORD)\
+    .option("driver", "org.postgresql.Driver")\
+    .mode("overwrite")\
+    .save()
 
 spark.stop()
